85c7f2a8d9f8df87394537c54684e558fbb19057
KSVQ <kristinasaricim@gmail.com>
2025-12-03 17:41:25 -0500

    Fix: Switch to non-privileged port 8000

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def page(slug):
     return render_template('post.html', content=html_content, title=slug)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8000)
